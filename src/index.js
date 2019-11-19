22afed3901f2553fee9a7ba15915a6ed458f6933
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2019-11-19 12:48:10 +0000

    first play around using jsdoc for create documentation

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * Name Of Person
+ * @type {string}
+*/
+
+const name = 'youssef';
+
+/**
+ * lists
+ * @type {Array<number>}
+ */
+
+ const listsOfNumbers = [1,8,48,47];
+
+/**
+  * @type {{id: number, text: string}}
+*/
+
+const object = {
+      id: 1,
+      text: 'hello docs'
+}
+
+/**
+ * type a name
+ * @param {string} name 
+ * @return {string} name
+ */
+
+function printName (name){
+    return 'My name is ' + ' ' + name;
+};
